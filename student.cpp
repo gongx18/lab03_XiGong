d88c15f1802379934fcde8808ce7c530cda49b69
Xi Gong <xi@ucsb.edu>
2019-01-27 19:47:20 -0800

    first

---
+++ student.cpp
@@ -0,0 +1,58 @@
+#include "student.h"
+#include <string>
+#include <cstring>
+
+Student::Student(const char * const name, int perm) {
+  this->setName("another stub");
+}
+
+int Student::getPerm() const {
+  return perm;
+}
+
+const char * const Student::getName() const {
+  return name; //???
+}
+
+void Student::setPerm(const int permNumber) {
+  perm = permNumber;//???
+}
+
+void Student::setName(const char * const name) {
+  this->name = new char[strlen(name)+1];
+  strcpy(this->name,name);
+}
+
+
+Student::Student(const Student &orig) {
+  this->setName(orig.getName());//???
+  this->setPerm(orig.getPerm());
+}
+
+Student::~Student() {
+   this->name = name; 
+   this->perm = perm; 
+}
+
+Student & Student::operator=(const Student &right) {
+  // The next two lines are standard, and you should keep them.
+  // They avoid problems with self-assignment where you might free up 
+  // memory before you copy from it.  (e.g. x = x)
+
+  if (&right == this) 
+    return (*this);
+
+  // TODO... Here is where there is code missing that you need to 
+  // fill in...
+
+
+  // KEEP THE CODE BELOW THIS LINE
+  // Overloaded = should end with this line, despite what the textbook says.
+  return (*this); 
+
+}
+
+std::string Student::toString() const {
+  return "tostring stub";
+}
+
---
+++ studentRoll.cpp
@@ -0,0 +1,75 @@
+#include <string>
+#include "studentRoll.h"
+
+StudentRoll::StudentRoll() {
+  head = tail = NULL;
+}
+
+void StudentRoll::insertAtTail(const Student &s) {//the parameter is a pointer to a Student object; 
+	auto n = new Node;
+
+	n->s = new Student(s);
+
+	if (head == nullptr)
+		head = n;
+
+	if (tail != nullptr)
+		tail->next = n;
+
+	tail = n;
+}
+
+std::string StudentRoll::toString() const {
+	std::string str = "[";
+
+	for (auto p = head; p != nullptr; p = p->next) {
+		str += p->s->toString();
+		if (p != tail)
+			str += ",";
+	}
+
+	str += "]";
+	return str;
+}
+
+StudentRoll::StudentRoll(const StudentRoll &orig) {
+    head = tail = nullptr;
+
+    for (auto n = orig.head; n != nullptr; n = n->next) //I copied this from online
+        insertAtTail(*(n->s));
+          
+}
+
+StudentRoll::~StudentRoll() {
+	for (auto current = head; current != nullptr;) {
+		const auto next = current->next;
+		delete current;
+		current = next;
+	}
+}
+
+StudentRoll & StudentRoll::operator =(const StudentRoll &right ) {
+  // The next two lines are standard, and you should keep them.
+  // They avoid problems with self-assignment where you might free up 
+  // memory before you copy from it.  (e.g. x = x)
+
+  if (&right == this) 
+    return (*this);
+
+  // TODO... Here is where there is code missing that you need to 
+  // fill in...
+  ~StudentRoll();
+  
+
+
+  // KEEP THE CODE BELOW THIS LINE
+  // Overloaded = should end with this line, despite what the textbook says.
+  return (*this); 
+  
+}
+
+
+
+
+
+
